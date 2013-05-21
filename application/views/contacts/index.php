3c523201a1c721944205821d85ddedc191f7287d
jensdeschrijver <jensdeschrijver@gmail.com>
2013-05-21 12:48:40 +0200

    Modified the dropdown menu button for each customer-line in the overview: added dashboard link as direct link and others to dropdown.

--- application/views/contacts/index.php
+++ application/views/contacts/index.php
@@ -31,30 +31,32 @@
 	$customernumber = $customer[param('param_asw_database_column_customernumber')];
 	?>
 	<div class="row">
-		<div class="span12">
-			<div class="row">
-				<div class="span2">
-					<p><?= $customer[param('param_asw_database_column_customernumber')] ?> <br /></p>
-				</div>
-				<div class="span4">
-					<p><b><?= utf8_encode($customer[param('param_asw_database_column_customername')]) ?></b> <br/></p>
-				</div>
-				<div class="align-right" style="padding-right: 10px;">
-						<div class="btn-group">
-							<a class="btn btn-success dropdown-toggle" data-toggle="dropdown" href="#"><i class="icon-cog icon-white"></i> <?php echo ucfirst($this->siebel->getLang('actions')); ?> <span class="caret"></span></a>
-							<ul class="dropdown-menu align-left pull-right">
-								<?php
-								echo $this->bootstrap->navItem('View contacts', 'contacts/customer', '/contacts/customer/'.$customernumber, 'contacts', FALSE, 'icon-envelope');
-								echo $this->bootstrap->navItem('View comments', 'comments/customer', '/comments/customer/'.$customernumber, 'comments', FALSE, 'icon-comment');
-								echo $this->bootstrap->navItem('View deliverydays', 'deliverydays/customer', '/deliverydays/customer/'.$customernumber, 'deliverydays', FALSE, 'icon-map-marker');
-								echo $this->bootstrap->navItem('View delivery terms', 'deliveryterms/customer', '/deliveryterms/customer/'.$customernumber, 'deliveryterms', FALSE, 'icon-calendar');
-								echo $this->bootstrap->navItem('View prices', 'prices/customer', '/prices/customer/'.$customernumber, 'prices', FALSE, 'icon-tags');
-								?>
-							</ul>
-						</div>
+			<div class="span12">
+				<div class="row">
+					<div class="span2">
+						<p><?php echo $customer[param('param_asw_database_column_customernumber')]; ?> <br /></p>
+					</div>
+					<div class="span4">
+						<p><b><?php echo utf8_encode($customer[param('param_asw_database_column_customername')]); ?></b> <br/></p>
+					</div>
+					<div class="align-right" style="padding-right: 10px;">
+							<div class="btn-group">
+								<a class="btn btn-success" href="<?php echo site_url('dashboard/customer/'.$customernumber); ?>"><i class="icon-th-large icon-white"></i> Dashboard</a>
+								<a class="btn btn-success dropdown-toggle" data-toggle="dropdown" href="#"><span class="caret"></span></a>
+								<ul class="dropdown-menu align-left pull-right">
+									<?php
+									echo $this->bootstrap->navItem('View contacts', 'contacts/customer', '/contacts/customer/'.$customernumber, 'contacts', FALSE, 'icon-envelope');
+									echo $this->bootstrap->navItem('View comments', 'comments/customer', '/comments/customer/'.$customernumber, 'comments', FALSE, 'icon-comment');
+									echo $this->bootstrap->navItem('View deliverydays', 'deliverydays/customer', '/deliverydays/customer/'.$customernumber, 'deliverydays', FALSE, 'icon-map-marker');
+									echo $this->bootstrap->navItem('View holidays', 'holidays/customer', '/holidays/customer/'.$customernumber, 'holidays', FALSE, 'icon-plane');
+									echo $this->bootstrap->navItem('View delivery terms', 'deliveryterms/customer', '/deliveryterms/customer/'.$customernumber, 'deliveryterms', FALSE, 'icon-calendar');
+									echo $this->bootstrap->navItem('View prices', 'prices/customer', '/prices/customer/'.$customernumber, 'prices', FALSE, 'icon-tags');
+									?>
+								</ul>
+							</div>
+					</div>
 				</div>
 			</div>
-		</div>
 	</div>
 			
 <?php } ?>
