19443a3c88c9c797a1b2cd634cc4129e0e1d645a
jensdeschrijver <jensdeschrijver@gmail.com>
2013-05-22 11:27:55 +0200

    Visibility of comment icon in dashboard widgets modified. Now only visible if comment is not empty.

--- application/widgets/holidays/view.php
+++ application/widgets/holidays/view.php
@@ -13,7 +13,7 @@
 		<div>
 			<div class="row-fluid">
 				<ul class="nav nav-pills">
-					<li class="span5"><a><?php echo ucfirst($this->siebel->getLang('from')); ?></a></li>
+					<li class="span4"><a><?php echo ucfirst($this->siebel->getLang('from')); ?></a></li>
 					<li class="span5"><a><?php echo ucfirst($this->siebel->getLang('until')); ?></a></li>
 				</ul>
 			</div>
@@ -23,15 +23,23 @@
 			<?php foreach($holidays_content as $holiday) { 
 			?>
 				<div class="row-fluid">
-					<div class="span5">
+					<div class="span4">
 						<p><?php echo date('d/m/Y',  mysql_to_unix($holiday->from)) ?><br /></p>
 					</div>
 					<div class="span5">
 						<p><?php echo date('d/m/Y',  mysql_to_unix($holiday->until)) ?><br /></p>
 					</div>
+					<div class="span1">
+						<p>
+							<?php if(!empty($holiday->comment) || $holiday->comment != '')
+							{
+								echo '<a href="#" class="comment" rel="tooltip" title="'.$holiday->comment.'"><i class="icon-comment"></i></a>';
+							};
+							?>
+						</p>
+					</div>
 					<div class="align-right tools">
 						<p>
-							<a href="#" class="comment" rel="tooltip" title="<?php echo $holiday->comment ?>"><i class="icon-comment"></i></a>
 							<a href="<?php echo site_url($boxId.'/customer/'.$customernumber.'/'. $holiday->id); ?>" class="edit"><i class="icon-pencil"></i></a>
 						</p>
 					</div>
